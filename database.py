646e360d198f2cbaaa056759845c1381e6eb66cd
Muhammad-Zunair-Khalid <zunairkhalid999@gmail.com>
2026-05-09 23:05:24 +0500

    Updated File

--- database.py
+++ database.py
@@ -7,7 +7,7 @@ from dotenv import load_dotenv
 from mysql.connector.pooling import MySQLConnectionPool
 
 load_dotenv()
-
+# pooling
 _DB_CONFIG = {
     "host": os.getenv("MYSQL_HOST", "localhost"),
     "port": int(os.getenv("MYSQL_PORT", "3306")),
